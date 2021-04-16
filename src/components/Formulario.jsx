a4cb5e36f5180c93b16c0566ec605c81dc200424
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-16 16:38:51 -0500

    [add] first form with Reactjs

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    const [nombre, setNombre] = useState('')
+    const [edad, setEdad] = useState('')
+    const [estado, setEstado] = useState([])
+
+    const validar = (event) => {
+        event.preventDefault()
+        console.log('pulsado el boton');
+        if (!nombre.trim()) {
+            console.log('nombre esta vacio');
+            return
+        }
+        if (!edad.trim()) {
+            console.log('Edad esta vacio');
+            return
+        }
+    }
+
     return (
         <div className="container">
-            <form className="form-group">
-                <input placeholder="Nombre" className="form-control mb-3" type="text" name="" id=""/>
-                <input placeholder="Edad" className="form-control mb-3" type="text" name="" id=""/>
-                <input className="btn btn-info w-100" type="submit" value="Enviar"/>
+            <form onSubmit={validar} className="form-group">
+                <input
+                    placeholder="Nombre"
+                    className="form-control mb-3"
+                    type="text"
+                    onChange={(e) => { setNombre(e.target.value) }}
+                />
+                <input
+                    placeholder="Edad"
+                    className="form-control mb-3"
+                    type="text"
+                    onChange={(e) => { setEdad(e.target.value) }}
+                />
+                <input
+                    className="btn btn-info w-100"
+                    type="submit"
+                    value="Enviar"
+                />
             </form>
         </div>
     )
